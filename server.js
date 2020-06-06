53edb0d5419900684e8ab10f8c945f6b526cb6d2
Lucas Azael Linhares Soares <azaelsoares@gmail.com>
2020-06-05 23:54:34 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+//Importando o express
+const express = require('express');
+
+//O microframework retorna uma função express
+const app = express();
+
+//Ouvindo porta do navegador
+app.listen(3001);
