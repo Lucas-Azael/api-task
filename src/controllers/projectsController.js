0f71e675faadb8f675f61ff5d845cd8dac1c2e2c
Lucas Azael Linhares Soares <azaelsoares@gmail.com>
2020-07-04 01:42:42 -0300

    adicionando jwt

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const authMidlleware = require('../middlewares/auth');
 
 const router = express.Router();
+
+router.use(authMidlleware);
 
 router.get('/', (req, res) => {
     res.send({ok: true});
